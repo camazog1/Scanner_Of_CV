03ebe444ad82b50a053c0d7fb4c37c26d23188ca
camazog1 <camazog1@eafit.edu.co>
2025-02-19 22:17:49 -0500

    File Upload

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from app.routes import scan 
+from fastapi.staticfiles import StaticFiles
+from app.routes import scan, upload, processing
 
 app = FastAPI()
 
 app.include_router(scan.router) 
+app.include_router(upload.router)
+app.include_router(processing.router)
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 @app.get("/")
 def read_root():
