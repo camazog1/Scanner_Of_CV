6a74fce273a8b7e1be27fa3097402ac62a3cc6fe
unknown <jdagii2004@gmail.com>
2025-03-07 17:12:35 -0500

    Home page buttons and view changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import '@styles/Index.css';
+
 function Header() {
     return (
-        <nav className="navbar navbar-expand-lg" style={{backgroundColor: 'var(--primary-color'}}>
+        <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'var(--primary-color)' }}>
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <Link to="/" className="navbar-brand text-white">Hirelens</Link>
+                <Link to="/" className="navbar-brand text-white">HireLens</Link>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
@@ -16,11 +17,14 @@ function Header() {
                         <li className="nav-item">
                             <Link to="/file-upload" className="nav-link text-white">File upload</Link>
                         </li>
+                        <li className="nav-item">
+                            <Link to="/guide" className="nav-link text-white">Guía</Link>  {/* Nuevo botón */}
+                        </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Header;
