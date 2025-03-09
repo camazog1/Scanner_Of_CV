e4a4ab602fb3bb7ee7dc26d3535a5e0a39eb63db
unknown <jdagii2004@gmail.com>
2025-03-09 17:19:19 -0500

    Code corrections

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,27 +13,27 @@ function Header() {
 
     return (
         <>
-            {/* Header adicional solo en móviles */}
+            
             {isMobile && (
                 <div className="mobile-header">
                     <img src="/images/magneto_logo.png" alt="Magneto Logo" className="magneto-logo-mobile" />
                 </div>
             )}
 
-            {/* Header principal */}
+            
             <nav className="navbar navbar-expand-lg header-container">
                 <div className="container-fluid">
-                    {/* Logo HireLens a la izquierda */}
+                    
                     <Link to="/" className="navbar-brand text-white">
                         <img src="/images/hirelens_logo.png" alt="HireLens Logo" style={{ height: "45px" }} />
                     </Link>
 
-                    {/* Botón de menú solo en móviles */}
+                    
                     <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    {/* Menú de navegación */}
+                    
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
@@ -46,7 +46,7 @@ function Header() {
                                 <Link to="/guide" className="nav-link text-white">Guía</Link>
                             </li>
 
-                            {/* Mostrar logo Magneto solo en PC */}
+                            
                             {!isMobile && (
                                 <li className="nav-item ms-4">
                                     <img src="/images/magneto_logo.png" alt="Magneto Logo" style={{ height: "40px" }} />
