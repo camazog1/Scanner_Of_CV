36532dce7bc62f2b1a1eb1e4e32dc261458f764b
Jose Tordecilla <70181603+JoseTor101@users.noreply.github.com>
2025-04-16 14:06:42 -0500

    Feat/render fields (#46)
    * Fixed render fields #2

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,9 +1,10 @@
 import Header from "@components/Header";
 import { Outlet } from "react-router-dom";
+import "@styles/mainLayout.css";
 
 function MainLayout() {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className="main-layout d-flex flex-column min-vh-100 h-auto">
       <Header />
       <div className="container flex-grow-1 d-flex p-4">
         <Outlet /> {/* This will render the current page */}
