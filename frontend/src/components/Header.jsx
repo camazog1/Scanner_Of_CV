5210473f672496308eb014a2c70b0c34b1324453
camazog1 <camazog1@eafit.edu.co>
2025-04-23 08:23:07 -0500

    Add Database Connection

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "@styles/Index.css";
+import "@styles/index.css";
 
 function Header() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
